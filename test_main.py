7d9393633a60fcdd6029e7e7df645a57286c2aea
Luiz Gustavo Silva de Lima <2501391@SALAS.AULAS>
2026-04-08 20:03:42 -0300

    Adiciona multiplicacao e teste quebrando

--- test_main.py
+++ test_main.py
@@ -7,3 +7,9 @@ def test_somar():
     response = client.get("/somar/10/20")
     assert response.status_code == 200
     assert response.json() == {"resultado": 30}
+
+def test_multiplicar():
+    response = client.get("/multiplicar/2/2")
+    assert response.status_code == 200
+    # ERRO INTENCIONAL: Esperando 5 em vez de 4
+    assert response.json() == {"resultado": 5}
